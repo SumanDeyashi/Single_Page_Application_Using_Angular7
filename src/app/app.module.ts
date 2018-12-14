ee0ca440302305ad5adeeb16b049624eb101b906
sumon.deyashi@gmail.com <sumon.deyashi@gmail.com>
2018-12-14 12:39:55 +0530

    Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { CredentialsComponent } from './credentials/credentials.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CredentialsComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/credentials/credentials.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-credentials',
+  templateUrl: './credentials.component.html',
+  styleUrls: ['./credentials.component.css']
+})
+export class CredentialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
